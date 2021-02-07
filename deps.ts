aaa20a9cad17a25fcdcc6b70af94ac22512fc6af
Tejas Agarwal <67542663+tejasag@users.noreply.github.com>
2021-02-07 17:29:38 +0530

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 export * from "https://deno.land/x/sqlite/mod.ts";
 export { Collection } from "https://deno.land/x/harmony/src/utils/collection.ts";
-export { Pool } from "https://deno.land/x/postgres/mod.ts";
-export { PoolClient } from "https://deno.land/x/postgres/client.ts";
+export { Pool } from "https://deno.land/x/postgres@0.6.0/mod.ts";
+export { PoolClient } from "https://deno.land/x/postgres@0.6.0/client.ts";
 export { ld as _ } from "https://deno.land/x/deno_lodash/mod.ts";
-export {QueryObjectResult} from "https://deno.land/x/postgres/query.ts";
+export {QueryObjectResult} from "https://deno.land/x/postgres@.6.0/query.ts";
