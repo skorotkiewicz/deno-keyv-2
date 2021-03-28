75a5dfcece7da4f72848edf2af5d4c9ce5ab2332
1chi <75403863+1chiSensei@users.noreply.github.com>
2021-03-28 21:13:48 +0800

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -3,4 +3,4 @@ export { Collection } from "https://deno.land/x/harmony/src/utils/collection.ts"
 export { Pool } from "https://deno.land/x/postgres@0.6.0/mod.ts";
 export { PoolClient } from "https://deno.land/x/postgres@0.6.0/client.ts";
 export { ld as _ } from "https://deno.land/x/deno_lodash/mod.ts";
-export {QueryObjectResult} from "https://deno.land/x/postgres@.6.0/query.ts";
+export {QueryObjectResult} from "https://deno.land/x/postgres@0.6.0/query.ts";
