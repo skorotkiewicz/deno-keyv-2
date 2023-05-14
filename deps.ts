9e8a93f2f7a8fad67f451223f1f5155178764ccf
Sebastian Korotkiewicz <skorotkiewicz@gmail.com>
2023-05-14 21:13:00 +0200

    deps versions

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export * from "https://deno.land/x/sqlite/mod.ts";
-export { Collection } from "https://deno.land/x/harmony/src/utils/collection.ts";
-export { Pool, Client } from "https://deno.land/x/postgres/mod.ts";
-export { PoolClient } from "https://deno.land/x/postgres/client.ts";
+export * from "https://deno.land/x/sqlite@v3.7.2/mod.ts";
+export { Collection } from "https://deno.land/x/harmony@v2.8.0/src/utils/collection.ts";
+export { Pool } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
+export { PoolClient } from "https://deno.land/x/postgres@v0.17.0/client.ts";
